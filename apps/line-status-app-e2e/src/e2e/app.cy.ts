52666fd98ff7566d8326196d355e194f2fa8239b
melihokursoy <melih.okursoy@gmail.com>
2024-05-06 09:29:47 +0100

    add cypress-axe for accesibility testing

--- apps/line-status-app-e2e/src/e2e/app.cy.ts
+++ apps/line-status-app-e2e/src/e2e/app.cy.ts
@@ -1,10 +1,24 @@
-import { getGreeting } from '../support/app.po';
+import { getH1, getUl } from '../support/app.po';
+import 'cypress-axe'
+
 
 describe('line-status-app-e2e', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display title', () => {
-
-    getGreeting().contains(/Status Updates/);
+    getH1().contains(/Status Updates/);
   });
+
+  it('Has an accesible unorderd list', () => {
+    getUl().contains(/Bakerloo/);
+    cy.injectAxe()
+    cy.checkA11y('ul');
+  });
+
+  it('Has an accesible heading', () => {
+    getH1().contains(/Status Updates/);
+    cy.injectAxe()
+    cy.checkA11y('h1');
+  });
+
 });
--- apps/line-status-app-e2e/src/support/app.po.ts
+++ apps/line-status-app-e2e/src/support/app.po.ts
@@ -1,1 +1,3 @@
-export const getGreeting = () => cy.get('h1');
+export const getH1 = () => cy.get('h1');
+export const getUl = () => cy.get('ul');
+
